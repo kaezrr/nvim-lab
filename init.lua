e7e2d92f9a731132c6909c68b70ae33afaa51001
Anjishnu Banerjee <inviktor27@gmail.com>
2025-04-19 11:58:33 +0530

    Update config structure

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@
 --    |____|__ (____  /\___  >_____ \ |__|
 --        \/    \/     \/      \/
 --
---    Courtesy to kickstart.nvim !!!
+--    Courtesy of kickstart.nvim !!!
 
 -- Set <space> as the leader key
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -13,13 +13,13 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- [[ Setting options ]]
-require 'options'
+require 'config.options'
 
 -- [[ Basic and high powered keybinds ]]
-require 'keybinds'
+require 'config.keybinds'
 
 -- [[ Bootstrap Lazy package manager and install plugins ]]
-require 'lazy-plugins'
+require 'config.lazy'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
