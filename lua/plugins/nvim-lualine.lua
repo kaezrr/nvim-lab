383ad1289421f33bcdf972f4da488561499eecce
Anjishnu Banerjee <inviktor27@gmail.com>
2025-04-19 18:08:21 +0530

    Final update for the day

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -1,23 +1,7 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/trouble.nvim' },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local trouble = require 'trouble'
-    local symbols = trouble.statusline {
-      mode = 'lsp_document_symbols',
-      groups = {},
-      title = false,
-      filter = { range = true },
-      format = '{kind_icon}{symbol.name:Normal}',
-      hl_group = 'lualine_c_normal',
-    }
-
-    require('lualine').setup {
-      sections = {
-        lualine_c = {
-          { symbols.get, cond = symbols.has },
-        },
-      },
-    }
+    require('lualine').setup {}
   end,
 }
