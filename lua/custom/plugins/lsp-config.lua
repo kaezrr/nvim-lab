ea224722baa50b481b1b46eb0d28fb28f344a9df
Anjishnu Banerjee <inviktor27@gmail.com>
2025-04-19 11:46:46 +0530

    Update lsp-config

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -194,18 +194,15 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
+      clangd = {},
+      rust_analyzer = {},
+      ts_ls = {},
+      cssls = {},
+      docker_compose_language_service = {},
+      emmet_language_server = {},
+      html = {},
+      tailwindcss = {},
+      yamlls = {},
 
       lua_ls = {
         -- cmd = { ... },
@@ -239,6 +236,9 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'beautysh',
+      'clang-format',
+      'prettier',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
